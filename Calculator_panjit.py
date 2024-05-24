56f08224c43a648c51a8cccb39ab239c126ad111
HolomanTW <157454689+HolomanTW@users.noreply.github.com>
2024-05-24 16:26:32 +0800

    完成 待驗收

--- Calculator_panjit.py
+++ Calculator_panjit.py
@@ -16,6 +16,7 @@ def animation():
 def loadfiles():
     global title
     global total_files
+    retry = None
 
     folder_path = filedialog.askdirectory()
  
@@ -34,10 +35,7 @@ def loadfiles():
 
             if 'Parameter' in header.columns:
                 title = [x for x in header.columns if x not in ban_list and 'CONT' not in x and 'OPEN' not in x]
-                files_amount.set(len(total_files))
-                test = threading.Thread(target=animation)
-                test.start()
-                root.mainloop()
+               
                 break
             else:
                 retry = messagebox.askretrycancel('Error','找不到測項標題\n\n請重新選擇')
@@ -46,34 +44,39 @@ def loadfiles():
                 else:
                     total_files = []
                     break
-        
+
+        if retry == False:
+            pass
+        else:
+            while True:
+                bin_header = pl.read_csv(total_files[0],has_header=True,skip_rows=bin_row.get()-1,n_rows=0,truncate_ragged_lines=True)
+                if 'Bin' in bin_header.columns:
+                    files_amount.set(len(total_files))
+                    test = threading.Thread(target=animation)
+                    test.start()
+                    root.mainloop()
+                    break
+                else:
+                    messagebox.showerror('Error',"找不到 Bin 標題\n\n請重新選擇")
+                    settings()
+
 
 
 def calculate():
     global percentage
-    #global rescancsv
     global limit_rd
-    #global rd
     global Robust_Mean
     global Robust_Sigma
     global spat_rd
     #global combine_forSPAT_rd
-    global total_rd
     global SPAT_sigmaX
-    global SPAT_lower_limit
-    global SPAT_upper_limit
 
     parameter.set(title)
     SPAT_sigmaX = [6]*len(title)
     
-
-    file_queue = []
-    for file in total_files:
-        rd = pl.scan_csv(file ,has_header=True,skip_rows=header_row.get()-1).drop('').filter(~pl.all_horizontal(pl.all().is_null())).filter(pl.col('Parameter') == 'PID-')
-        file_queue.append(rd)
-    total_rd = pl.collect_all(file_queue)
+    rd = pl.scan_csv(total_files ,has_header=True,skip_rows=header_row.get()-1).drop('').filter(~pl.all_horizontal(pl.all().is_null())).filter(pl.col('Parameter') == 'PID-')
     limit_rd = pl.read_csv(total_files[0],has_header=True,skip_rows=header_row.get()-1,skip_rows_after_header=1,n_rows=3).drop('')
-    combine_forSPAT_rd = pl.concat(total_rd).filter(pl.col('Bin#') == '1').drop('Parameter')
+    combine_forSPAT_rd = rd.filter(pl.col('Bin#') == '1').drop('Parameter').collect()
 
 
     #轉換數據類型為Float
@@ -88,74 +91,34 @@ def calculate():
     except TypeError:
         messagebox.showerror('Error','無良率')
 
-    for parameter_ in title:
-        MIN_rd = limit_rd.filter(pl.col('Parameter') == 'Min').select(parameter_).item()   #問題
-        if MIN_rd == None:
-            MIN = MIN_rd
-        else:
-            try:
-                MIN = float(MIN_rd)
-            except:
-                MIN = ''.join(filter(lambda x: x.isdigit() or x == '.' or x == '-',MIN_rd))
 
-        MAX_rd = limit_rd.filter(pl.col('Parameter') == 'Max').select(parameter_).item()   #問題
-        if MAX_rd == None:
-            MAX = MAX_rd
-        else:
-            try:
-                MAX = float(MAX_rd)
-            except:
-                MAX = ''.join(filter(lambda x: x.isdigit() or x == '.' or x == '-',MAX_rd))
-
-        upper_limit = Robust_Mean.select(parameter_).item() + 6 * Robust_Sigma.select(parameter_).item()
-        lower_limit = Robust_Mean.select(parameter_).item() - 6 * Robust_Sigma.select(parameter_).item()
-
-        if MAX == None:
-            SPAT_upper_limit.append(round(upper_limit,3))
-        elif upper_limit > MAX:
-            SPAT_upper_limit.append(MAX)
-        else:
-            SPAT_upper_limit.append(round(upper_limit,3))
-            
-        if MIN == None:
-            SPAT_lower_limit.append(round(lower_limit,3))
-        elif lower_limit < MIN:
-            SPAT_lower_limit.append(MIN)
-        else:
-            SPAT_lower_limit.append(round(lower_limit,3))
-
-    print(SPAT_upper_limit)
-    print(SPAT_lower_limit)
-                
-
-'''    if recalculatesbl == True:
-        for file in total_files:
-            bin_rd = pl.read_csv(file ,has_header=True,skip_rows=bin_row.get()-1,skip_rows_after_header=1,n_rows=1,truncate_ragged_lines=True)
-            bin_rd.drop_in_place('Bin')
-            bin_rd.drop_in_place('')
-            bin_rd = bin_rd.drop((i.name for i in bin_rd if (i.null_count() == 1)))
-        
-            try:
-                bin_data_base = pl.concat([bin_data_base,bin_rd],how='diagonal').fill_null(0)
-            except:
-                bin_data_base = bin_rd
-        bin_data_base_cast = bin_data_base.cast(pl.Int32)
-        qty_sum = bin_data_base_cast.sum_horizontal()
-        percentage = bin_data_base_cast / qty_sum
-        recalculatesbl = False
-
-        #SYL計算
-        SYL_Mean = percentage['1'].mean() *100
-        SYL_Sigma = percentage['1'].std() *100
-        
-        #輸出SYL    
-        SYL.set(round(SYL_Mean - 3 * SYL_Sigma , 3))
-        SYL_1.set(round(SYL_Mean - 4 * SYL_Sigma , 3))
-        syl_display.set(round(SYL_Mean,3))
-        syl_display1.set(round(SYL_Sigma,3))
-        percentage.drop_in_place('1')
-        sbl_btn.config(state=tk.NORMAL)'''
+    for file in total_files:
+        bin_rd = pl.read_csv(file ,has_header=True,skip_rows=bin_row.get()-1,skip_rows_after_header=1,n_rows=1,truncate_ragged_lines=True)
+        bin_rd.drop_in_place('Bin')
+        bin_rd.drop_in_place('')
+        bin_rd = bin_rd.drop((i.name for i in bin_rd if (i.null_count() == 1)))
     
+        try:
+            bin_data_base = pl.concat([bin_data_base,bin_rd],how='diagonal').fill_null(0)
+        except:
+            bin_data_base = bin_rd
+    bin_data_base_cast = bin_data_base.cast(pl.Int32)
+    qty_sum = bin_data_base_cast.sum_horizontal()
+    percentage = bin_data_base_cast / qty_sum
+
+
+    #SYL計算
+    SYL_Mean = percentage['1'].mean() *100
+    SYL_Sigma = percentage['1'].std() *100
+    
+    #輸出SYL    
+    SYL.set(round(SYL_Mean - 3 * SYL_Sigma , 3))
+    SYL_1.set(round(SYL_Mean - 4 * SYL_Sigma , 3))
+    syl_display.set(round(SYL_Mean,3))
+    syl_display1.set(round(SYL_Sigma,3))
+    percentage.drop_in_place('1')
+    sbl_btn.config(state=tk.NORMAL)
+
     
     #SPAT計算DEBUG
 
@@ -205,28 +168,22 @@ def spat(category):
     #輸出SPAT
     if MAX == None:
         SPAT.set(round(upper_limit,3))
-        SPAT_upper_limit[n] = round(upper_limit,3)
         btn.config(fg='black')
     elif upper_limit > MAX:
         SPAT.set(MAX)
-        SPAT_upper_limit[n] = MAX
         btn.config(fg='red')
     else:
         SPAT.set(round(upper_limit,3))
-        SPAT_upper_limit[n] = round(upper_limit,3)
         btn.config(fg='black')
     
     if MIN == None:
         SPAT_1.set(round(lower_limit,3))
-        SPAT_lower_limit[n] = round(lower_limit,3)
         btn_1.config(fg='black')
     elif lower_limit < MIN:
         SPAT_1.set(MIN)
-        SPAT_lower_limit[n] = MIN
         btn_1.config(fg='red')
     else:
         SPAT_1.set(round(lower_limit,3))
-        SPAT_lower_limit[n] = round(lower_limit,3)
         btn_1.config(fg='black')
 
     count = spat_rd.select(listbox_parameter).count().item()
@@ -234,20 +191,10 @@ def spat(category):
     loss.set(f"{round((loss_die / count)*100,3)}%")
     print(f"total{count}")
     print(f'choose{loss_die}')
-    print(SPAT_upper_limit)
-    print(SPAT_lower_limit)
 
-def dpat():
-    for i,rd in enumerate(total_rd):
-        count_total = rd.count().item(0,0)
-    
-        print(count_total)
-    return
 def clean():
     global total_files
-    global listbox_status
     global percentage
-    #global rd
     global limit_rd
     global Robust_Mean
     global Robust_Sigma
@@ -262,13 +209,11 @@ def clean():
     syl_display.set(0)
     syl_display1.set(0)
     files_amount.set(0)
-    listbox_status = False
     sbl_btn.config(state=tk.DISABLED)
     loss.set('0.0%')
 
     try:
         del percentage
-        #del rd
         del limit_rd
         del Robust_Mean
         del Robust_Sigma
@@ -353,6 +298,10 @@ def settings():
     parameter_header_row.place(relx=0.1,rely=0.1)
     parameter_header_row_spinbox = tk.Spinbox(Setting,from_=0,to=100,font=('Arial',17),fg='#f00',justify='center',textvariable=header_row)
     parameter_header_row_spinbox.place(relx=0.5,rely=0.1,width=50)
+    bin_header_row = tk.Label(Setting,text='bin標題 row 定位',font=(10))
+    bin_header_row.place(relx=0.1,rely=0.3)
+    bin_header_row_spinbox = tk.Spinbox(Setting,from_=0,to=100,font=('Arial',17),fg='#f00',justify='center',textvariable=bin_row)
+    bin_header_row_spinbox.place(relx=0.5,rely=0.3,width=50)
 
     def close():
         Setting.destroy()
@@ -370,7 +319,7 @@ if __name__ == '__main__':
     root.resizable(False, False)
     window_width = root.winfo_screenwidth()    # 取得螢幕寬度
     window_height = root.winfo_screenheight()  # 取得螢幕高度
-    width = 1000
+    width = 800
     height = 600
     left = int((window_width - width)/2)       # 計算左上 x 座標
     top = int((window_height - height)/2)      # 計算左上 y 座標
@@ -385,7 +334,6 @@ if __name__ == '__main__':
     loss.set('0.0%')
     parameter = tk.StringVar()
     files_amount = tk.IntVar()
-    #files_amount.set("Files Count : 0")
     syl_display = tk.DoubleVar()
     syl_display1 = tk.DoubleVar()
     adj = tk.IntVar()
@@ -394,97 +342,81 @@ if __name__ == '__main__':
     header_row.set(19)
     bin_row = tk.IntVar()
     bin_row.set(14)
-    SPAT_upper_limit = []
-    SPAT_lower_limit = []
     
     ban_list = ['Parameter','X','Y','Bin#','Site#','B-CONT','C-CONT','E-CONT','OPEN/SHORT']
-    listbox_status = False
-    
-    
-    
+
     
     #元件配置初始化
-    #canvas
-    root_canvas = tk.Canvas(root,width=590,height=290)
-    root_canvas.place(relx=0,rely=0.5)
-    #root_canvas.create_line(40,40,560,40,width=3,)
-    #root_canvas.create_line(40,130,560,130,width=3)
-    #root_canvas.create_line(40,220,560,220,width=3)
     
     #SPAT標題
     label_spat = tk.Label(root, text='Upper\n\n\nLower\n\n\nSigma\n\n\nLoss %', font=('Arial',14))
     label_spat.place(x=50,y=300)
     label = tk.Label(root, text="SPAT",font=('Arial',20,'bold'))
-    label.place(relx=0.1,rely=0.39,height=50,width=190)
+    label.place(x=100,y=234,height=50,width=190)
     #SPAT按鈕
     btn = tk.Button(root,textvariable=SPAT, command=partial(copy,SPAT),font=('Arial',18),relief='solid',bd=2)
-    btn.place(relx=0.14,rely=0.48,width=110,height=50)
+    btn.place(x=140,y=288,width=110,height=50)
     #SPAT按鈕_1
     btn_1 = tk.Button(root,textvariable=SPAT_1, command=partial(copy,SPAT_1),font=('Arial',18),relief='solid',bd=2)
-    btn_1.place(relx=0.14,rely=0.6,width=110,height=50)
-    #DPAT標題
-    btn2 = tk.Button(root,text='DPAT',wraplength=300,font=('Arial',20,'bold'),relief='flat',command=dpat)
-    btn2.place(relx=0.3,rely=0.39,width=190,height=50)
+    btn_1.place(x=140,y=360,width=110,height=50)
+
     #SYL標題
     label1 = tk.Label(root, text="SYL", wraplength=300,font=('Arial',20,'bold'))
-    label1.place(relx=0.5,rely=0.39,width=190,height=50)
+    label1.place(x=300,y=234,width=190,height=50)
     #SYL按鈕
     btn1 = tk.Button(root,textvariable=SYL, command=partial(copy,SYL),font=('Arial',18),relief='solid',bd=2)
-    btn1.place(relx=0.54,rely=0.48,width=110,height=50)
+    btn1.place(x=340,y=288,width=110,height=50)
     #SYL按鈕_1
     btn1_1 = tk.Button(root,textvariable=SYL_1, command=partial(copy,SYL_1),font=('Arial',18),relief='solid',bd=2)
-    btn1_1.place(relx=0.54,rely=0.6,width=110,height=50)
+    btn1_1.place(x=340,y=360,width=110,height=50)
     #SYL title
     label3 = tk.Label(root,text='Mean\n\n\nSigma',font=('Arial',14))
-    label3.place(relx=0.68,rely=0.72)
+    label3.place(x=280,y=432)
     #SYL標註
     label4 = tk.Label(root,textvariable=syl_display,font=('Arial',20),anchor='center')
-    label4.place(relx=0.54,rely=0.71,width=110,height=50)
+    label4.place(x=340,y=426,width=110,height=50)
     label5 = tk.Label(root,textvariable=syl_display1,font=('Arial',20),anchor='center')
-    label5.place(relx=0.54,rely=0.81,width=110,height=50)
+    label5.place(x=340,y=486,width=110,height=50)
     #SBL標題
     label6 = tk.Label(root,text='SBL', wraplength=300,font=('Arial',20,'bold'))
-    label6.place(relx=0.7,rely=0.39,width=190,height=50)
+    label6.place(x=500,y=234,width=190,height=50)
     #SBL視窗按鈕
     sbl_btn = tk.Button(root, text="SBL", font=('Arial',20),command=sbl_window,relief='solid',bd=2,state=tk.DISABLED)
-    sbl_btn.place(relx=0.75,rely=0.48,width=100,height=50)    
+    sbl_btn.place(x=550,y=288,width=100,height=50)    
     #打開資料夾按鈕
     openfilesbtn = tk.Button(root,text='Open directory',font=('Arial',18),command=loadfiles,relief='solid',bd=2)
-    openfilesbtn.place(relx=0.1,rely=0.07,width=190,height=90)
+    openfilesbtn.place(x=100,y=42,width=190,height=90)
     #clean按鈕
     clnbtn = tk.Button(root ,text='Clean',font=('Arial',20),command=clean,relief='solid',bd=2)
-    clnbtn.place(relx=0.1,rely=0.23,width=190,height=90)
+    clnbtn.place(x=100,y=138,width=190,height=90)
     #計算按鈕
     runframe = tk.LabelFrame(root,bd=6,bg='red',relief='flat')
-    runframe.place(relx=0.5,rely=0.07,width=190,height=185)
+    runframe.place(x=300,y=138,width=190,height=90)
     runbtn = tk.Button(runframe ,text='Calculate',font=('Arial',20,'bold'),command=calculate,relief='flat')
     runbtn.pack(fill='both',expand=1)
     #滾動條
     scollbar = tk.Scrollbar(root)
-    scollbar.place(relx=0.9,rely=0.07,height=185)
+    scollbar.place(x=700,y=42,height=185)
     #listbox
     listbox = tk.Listbox(root ,listvariable=parameter,font=('Arial',10),selectmode='single',yscrollcommand=scollbar.set,justify='center',relief='solid',bd=2)
-    listbox.place(relx=0.7,rely=0.07,width=200,height=185)
+    listbox.place(x=500,y=42,width=200,height=185)
     listbox.bind('<<ListboxSelect>>',spat)
     #scollbar
     scollbar.config(command=listbox.yview)
     #檔案數量
     files_amount_frame = tk.LabelFrame(root,text='Files Count',relief='solid',bd=2,font=('Arial',17),labelanchor='n')
-    files_amount_frame.place(relx=0.3,rely=0.05,width=190,height=198)
+    files_amount_frame.place(x=300,y=30,width=190,height=100)
     files_amount_label = tk.Label(files_amount_frame,textvariable=files_amount, font=('Arial',40))
     files_amount_label.pack(fill='both',expand=1)
     #SPAT數值調整
     spinbox = tk.Spinbox(root,from_=0,to=50,font=('Arial',20),fg='#f00',justify='center',textvariable=adj,command=partial(spat,1))
-    spinbox.place(relx=0.14,rely=0.72,width=110)
+    spinbox.place(x=140,y=432,width=110)
     #設定
     menubar = tk.Menu(root)
     menubar.add_command(label='Settings',command=settings)
     root.config(menu=menubar)
     #LOSS率
     losslabel = tk.Label(root ,textvariable=loss,font=('Arial',20))
-    losslabel.place(relx=0.14,rely=0.81,width=110,height=50)
-    #DPAT狀態顯示
-    DPAT_Dis = tk.Text(root)
-    DPAT_Dis.place(x=300,y=285,width=190,height=280)
+    losslabel.place(x=140,y=486,width=110,height=50)
 
     root.mainloop()
